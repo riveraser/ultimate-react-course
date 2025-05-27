c756be710fc1c680878a6b0958dd81693f8784fc
Sergio Rivera <srivera@jpglobaldigital.com>
2025-05-26 22:46:25 -0500

    feat: Lecture 42 Passing and Receiving Props

--- pizza-menu-sr/src/index.js
+++ pizza-menu-sr/src/index.js
@@ -70,10 +70,32 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Margherita"
+        ingredients="Tomato and mozarella"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        photoName="pizzas/funghi.jpg"
+        price={12}
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price * 1.1}</span>
+      </div>
+    </div>
   );
 }
 
@@ -85,19 +107,8 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen ? " We're currently open!" : "We're currently closed!"}
+      {isOpen ? "We're currently open!" : "We're currently closed!"}
     </footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h3>Pizza</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-      <span>10$</span>
-    </div>
   );
 }
 
