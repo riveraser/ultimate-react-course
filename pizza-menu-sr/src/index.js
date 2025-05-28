4afa9b95d8b2bd0b1922c2feca904be4a35c49f9
Sergio Rivera <srivera@jpglobaldigital.com>
2025-05-27 20:53:07 -0500

    feat: Lecture 48 Conditional Rendering with Ternaries

--- pizza-menu-sr/src/index.js
+++ pizza-menu-sr/src/index.js
@@ -74,12 +74,14 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzaData.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later :-) </p>
       )}
     </main>
   );
@@ -106,11 +108,16 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order Now</button>
         </div>
+      ) : (
+        <p>
+          We're currently closed. Come back between {openHour}:00 and{" "}
+          {closeHour}:00.
+        </p>
       )}
     </footer>
   );
