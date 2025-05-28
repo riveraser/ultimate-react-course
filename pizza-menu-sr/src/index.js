d3366c2912958ab5e0e12fefcbc5d598168bb5d5
Sergio Rivera <srivera@jpglobaldigital.com>
2025-05-27 20:48:43 -0500

    feat: Lecture 47 Conditional Rendering

--- pizza-menu-sr/src/index.js
+++ pizza-menu-sr/src/index.js
@@ -67,14 +67,20 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
@@ -100,7 +106,12 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen ? "We're currently open!" : "We're currently closed!"}
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order Now</button>
+        </div>
+      )}
     </footer>
   );
 }
