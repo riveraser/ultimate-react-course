aa339e052fc723666eef7eee75265c3f2c87f166
Sergio Rivera <srivera@jpglobaldigital.com>
2025-05-27 21:29:39 -0500

    feat: Lecture 55 improve the profile card

--- my-profile-card/src/components/Skill.js
+++ my-profile-card/src/components/Skill.js
@@ -1,16 +1,15 @@
 import * as IoIcons from "react-icons/io5";
 
-export default function Skill(props) {
+export default function Skill({ icon, bgColor, name, level }) {
   // Get the icon component dynamically
-  const IconComponent = IoIcons[props.icon];
+  const IconComponent = IoIcons[icon];
 
   return (
-    <div
-      className="skill"
-      key={props.name}
-      style={{ backgroundColor: props.bgColor }}
-    >
-      {props.name.toUpperCase()} {IconComponent && <IconComponent />}
+    <div className="skill" key={name} style={{ backgroundColor: bgColor }}>
+      {name.toUpperCase()} {IconComponent && <IconComponent />}
+      <span className="level">{level === "begginer" && "👶"}</span>
+      <span className="level">{level === "intermediate" && "👍"}</span>
+      <span className="level">{level === "advanced" && "💪"}</span>
     </div>
   );
 }
--- my-profile-card/src/components/SkillList.js
+++ my-profile-card/src/components/SkillList.js
@@ -1,15 +1,55 @@
 import Skill from "./Skill";
 
 const technologies = [
-  { name: "HTML + CSS", icon: "IoLogoHtml5", bgColor: "#e34c26" },
-  { name: "JavaScript", icon: "IoLogoJavascript", bgColor: "#f0db4f" },
-  { name: "Node.js", icon: "IoLogoNodejs", bgColor: "#339933" },
-  { name: "TypeScript", icon: "IoLogoMicrosoft", bgColor: "#3178c6" },
-  { name: "Vue.js", icon: "IoLogoVue", bgColor: "#4fc08d" },
-  { name: "NestJS", icon: "IoServerSharp", bgColor: "#e0234e" },
-  { name: "AWS", icon: "IoLogoAmazon", bgColor: "#f09933" },
-  { name: "Docker", icon: "IoLogoDocker", bgColor: "#2496ed" },
-  { name: "And now React!", icon: "IoLogoReact", bgColor: "#61dafb" },
+  {
+    name: "HTML + CSS",
+    icon: "IoLogoHtml5",
+    bgColor: "#e34c26",
+    level: "advanced",
+  },
+  {
+    name: "JavaScript",
+    icon: "IoLogoJavascript",
+    bgColor: "#f0db4f",
+    level: "advanced",
+  },
+  {
+    name: "Node.js",
+    icon: "IoLogoNodejs",
+    bgColor: "#339933",
+    level: "advanced",
+  },
+  {
+    name: "TypeScript",
+    icon: "IoLogoMicrosoft",
+    bgColor: "#3178c6",
+    level: "advanced",
+  },
+  { name: "Vue.js", icon: "IoLogoVue", bgColor: "#4fc08d", level: "advanced" },
+  {
+    name: "NestJS",
+    icon: "IoServerSharp",
+    bgColor: "#e0234e",
+    level: "advanced",
+  },
+  {
+    name: "AWS",
+    icon: "IoLogoAmazon",
+    bgColor: "#f09933",
+    level: "intermediate",
+  },
+  {
+    name: "Docker",
+    icon: "IoLogoDocker",
+    bgColor: "#2496ed",
+    level: "intermediate",
+  },
+  {
+    name: "And now React!",
+    icon: "IoLogoReact",
+    bgColor: "#61dafb",
+    level: "begginer",
+  },
 ];
 
 export default function SkillList() {
@@ -21,6 +61,7 @@ export default function SkillList() {
           name={technology.name}
           icon={technology.icon}
           bgColor={technology.bgColor}
+          level={technology.level}
         />
       ))}
     </div>
